d4a1645f9217f48cdf382ad860011413ce9e65e6
Septian Feri Ferdinan <feri.ferdinan79@gmail.com>
2019-10-29 21:24:21 +0700

    animate title

--- js/theme.js
+++ js/theme.js
@@ -8,6 +8,7 @@
 			$('.wrapperLoading').fadeOut('slow');
 		}, 1600)
 	})
+
 	$(".nav a").on("click", function () {
 		$(".nav").find(".active").removeClass("active");
 		$(this).parent().addClass("active");
