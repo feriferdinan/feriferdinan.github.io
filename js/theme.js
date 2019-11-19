d6dc9aa5835d0d6840147813b5a674bdc2ac32e6
Septian Feri Ferdinan <feri.ferdinan79@gmail.com>
2019-11-19 14:21:34 +0700

    responsive title

--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,7 @@
 (function ($) {
 	'use strict';
 	//loading
+	let title = $('title')
 	$('body').css("overflow", "hidden");
 	$(window).on('load', function () {
 		setTimeout(function () {
@@ -60,6 +61,19 @@
 		}
 
 	})
+	// check if mobile user
+	function detectmob() {
+		return (navigator.userAgent.match(/Android/i)
+			|| navigator.userAgent.match(/webOS/i)
+			|| navigator.userAgent.match(/iPhone/i)
+			|| navigator.userAgent.match(/iPad/i)
+			|| navigator.userAgent.match(/iPod/i)
+			|| navigator.userAgent.match(/BlackBerry/i)
+			|| navigator.userAgent.match(/Windows Phone/i)) ? true : false;
+	}
+	// change title if mobile browser
+	(detectmob()) ? title.text('Septian Feri Ferdinan') : title.text('eptian Feri Ferdinan')
+
 	var nav_offset_top = $('header').height() + 50;
 	/*-------------------------------------------------------------------------------
 	  Navbar 
